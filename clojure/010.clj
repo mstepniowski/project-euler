215993c56722c396f8a9707f8f815c9d0336acdf
zuber <marek@stepniowski.com>
2010-12-23 15:06:02 +0100

    Added solution to problem 10 in Clojure.

---
+++ clojure/010.clj
@@ -0,0 +1,12 @@
+;; Solution to Project Euler problem 10
+;; http://projecteuler.net/index.php?section=problems&id=10
+;;
+;; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+;;
+;; Find the sum of all the primes below two million.
+(ns com.stepniowski.project-euler
+  (:use clojure.contrib.lazy-seqs))
+
+(defn euler-10 []
+  (let [primes-below-2m (take-while #(< % 2000000) primes)]
+    (reduce + primes-below-2m)))
